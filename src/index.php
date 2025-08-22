a47ed3a2a87bc732474cb5e7c8521dbb202d0e44
KevinLIOUST <kevin.lioust@outlook.fr>
2025-08-22 12:07:46 +0200

    Page de connexion avance méga-bien  ! :)

---
+++ src/index.php
@@ -0,0 +1,5 @@
+<?php
+header('Location: login.php');
+echo "let's start !!!!";
+
+?>
---
+++ src/login.php
@@ -0,0 +1,103 @@
+<?php
+    require_once 'users.php';
+
+    var_dump($_POST);
+
+    // On lance uniquement quand il y a un formulaire validé via la méthode SESSION
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $errors = [];
+
+        if (isset($_POST['email'])) {
+            // On va vérifier si c'est vide
+            if (empty($_POST['email'])) {
+                // je crée une erreur dans mon tableau
+                $errors['email'] = 'Mail obligatoire';
+            } elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+                $errors['email'] = 'Mail non valide';
+            } else {
+                for ($i = 0; $i < count($users); $i++) {
+                    if ($_POST['email'] == $users[$i]['mail']) {
+                        $email = $_POST['email'];
+                        $_SESSION['email'] = $email;
+                        break;
+                    } elseif ($_POST['email'] != $users[$i]['mail']) {
+                        $errors['email'] = 'Mail incorrect';
+                    }
+                }
+            }
+        }
+
+        if (isset($_POST['mdp'])) {
+            $mdp = $_POST['mdp'];
+            // On va vérifier si c'est vide
+            if (empty($_POST['mdp'])) {
+                // je crée une erreur dans mon tableau
+                $errors['mdp'] = 'Mot de passe obligatoire';
+            } else {
+                for ($i = 0; $i < count($users); $i++) {
+                    if ($_POST['mdp'] == $users[$i]['password']) {
+                        $mdp = $_POST['mdp'];
+                        $_SESSION['mdp'] = $mdp;
+                        break;
+                    } elseif ($_POST['mdp'] != $users[$i]['password']) {
+                        $errors['mdp'] = 'Mot de passe incorrect';
+                    }
+                }
+            }
+        }
+
+        var_dump($_SESSION);
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Session !!!!</title>
+
+    <!-- Lien vers Bootstrap -->
+    <link rel="stylesheet" href="../../node_modules/bootstrap/dist/css/bootstrap.min.css" />
+
+    <!-- Lien vers les icônes Bootstrap -->
+    <link rel="stylesheet" href="../../node_modules/bootstrap-icons/font/bootstrap-icons.min.css">
+
+    <!-- Lien vers le fichier pour designer le site web -->
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div class="d-flex justify-content-center mt-3">
+        <h1>Entrainement pour la connexion à un compte admin ou utilisateur normal !!!!</h1>
+    </div>
+    <form action="" method="POST">
+        <div class="d-flex justify-content-center">
+            <div>
+                <div>
+                    <label class="text-left" for="email">Adresse Email <span class="text-danger">*</span><span class="text-danger"><?= isset($errors['email']) ? $errors['email'] : '' ?></span></label>
+                </div>
+                <div class="text-center">
+                    <input class="mt-1 taille-email design-email" id="email" type="text" name="email" placeholder="email" value="<?= $_POST[
+                    'email'
+                ] ?? '' ?>">
+                </div>
+            </div>
+        </div>
+        <div class="d-flex justify-content-center">
+            <div>
+                <div>
+                    <label class="mt-3 text-left" for="mdp">Mot de passe <span class="text-danger">*</span><span class="text-danger"><?= isset($errors['mdp']) ? $errors['mdp'] : '' ?></span></label>
+                </div>
+                <div class="text-center">
+                    <input class="mt-1 taille-mdp design-mdp" id="mdp" type="password" name="mdp" placeholder="mot de passe" value="<?= $_POST[
+                    'mdp'
+                ] ?? '' ?>">
+                </div>
+            </div>
+        </div>
+        <div class="d-flex justify-content-center mt-4">
+            <input type="submit" class="btn btn-connexion" value="Se connecter">
+        </div>
+    </form>
+</body>
+</html>
